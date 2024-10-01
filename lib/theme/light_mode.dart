5095847e57fc1152cf5b275ce0a8ad0dc2617e1d
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-10-01 23:47:12 +0100

    Add light mode theme data

---
+++ lib/theme/light_mode.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightMode = ThemeData(
+  colorScheme: const ColorScheme.light(
+    surface: Color(0xFFF7F0BA),
+    primary: Color.fromARGB(255, 169, 203, 166),
+    secondary: Color.fromARGB(255, 126, 190, 163),
+    inversePrimary: Color.fromARGB(255, 83, 160, 142),
+  ),
+);
