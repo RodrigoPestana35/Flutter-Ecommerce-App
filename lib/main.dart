d18daf3daae89fec77760ebc74e61ce90d0d4f12
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-10-02 17:25:10 +0100

    Refactor main.dart to add Shop model and cart functionality

--- lib/main.dart
+++ lib/main.dart
@@ -1,11 +1,18 @@
+import 'package:ecommerceapp/models/shop.dart';
 import 'package:ecommerceapp/pages/cart_page.dart';
 import 'package:ecommerceapp/pages/intro_page.dart';
 import 'package:ecommerceapp/pages/shop_page.dart';
 import 'package:ecommerceapp/theme/light_mode.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    ChangeNotifierProvider(
+      create: (context) => Shop(),
+      child: const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
