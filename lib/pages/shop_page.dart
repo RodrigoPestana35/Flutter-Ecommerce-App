2121480315e9aa9060139d1e9c3533a23fa08aea
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-10-02 17:27:58 +0100

    Refactor shop_page.dart to add product listing functionality

--- lib/pages/shop_page.dart
+++ lib/pages/shop_page.dart
@@ -1,11 +1,18 @@
 import 'package:ecommerceapp/components/my_drawer.dart';
+import 'package:ecommerceapp/components/my_product_tile.dart';
+import 'package:ecommerceapp/main.dart';
+import 'package:ecommerceapp/models/shop.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
 
 class ShopPage extends StatelessWidget {
   const ShopPage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    //acess products
+    final products = context.watch<Shop>().shop;
+
     return Scaffold(
       appBar: AppBar(
         title: const Text("Shop Page"),
@@ -13,8 +20,18 @@ class ShopPage extends StatelessWidget {
         elevation: 0,
         foregroundColor: Theme.of(context).colorScheme.inversePrimary,
       ),
-      drawer: MyDrawer(),
+      drawer: const MyDrawer(),
       backgroundColor: Theme.of(context).colorScheme.surface,
+      body: ListView.builder(
+        itemCount: products.length,
+        itemBuilder: (context, index) {
+          //get each product
+          final product = products[index];
+
+          //return a ProductTile
+          return MyProductTile(product: product);
+        },
+      ),
     );
   }
 }
