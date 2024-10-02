8093a109c775b8ec560f6b09c977cd7d6a2cedfb
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-10-02 18:09:02 +0100

    Refactor CartPage UI and functionality

--- lib/pages/cart_page.dart
+++ lib/pages/cart_page.dart
@@ -1,3 +1,4 @@
+import 'package:ecommerceapp/components/my_button.dart';
 import 'package:ecommerceapp/models/product.dart';
 import 'package:ecommerceapp/models/shop.dart';
 import 'package:flutter/material.dart';
@@ -10,30 +11,44 @@ class CartPage extends StatelessWidget {
   void removeFromCart(BuildContext context, Product product) {
     //show a dialog box to ask the user if they want to remove the product from the cart
     showDialog(
-        context: context,
-        builder: (context) {
-          return AlertDialog(
-            content: Text("Do you want to remove this product from the cart?"),
-            actions: [
-              //yes button
-              MaterialButton(
-                onPressed: () {
-                  //remove product from cart
-                  context.read<Shop>().removeFromCart(product);
-                  Navigator.pop(context);
-                },
-                child: Text("Yes"),
-              ),
-              //cancel button
-              MaterialButton(
-                onPressed: () {
-                  Navigator.pop(context);
-                },
-                child: Text("Cancel"),
-              ),
-            ],
-          );
-        });
+      context: context,
+      builder: (context) {
+        return AlertDialog(
+          content: Text("Do you want to remove this product from the cart?"),
+          actions: [
+            //yes button
+            MaterialButton(
+              onPressed: () {
+                //remove product from cart
+                context.read<Shop>().removeFromCart(product);
+                Navigator.pop(context);
+              },
+              child: Text("Yes"),
+            ),
+            //cancel button
+            MaterialButton(
+              onPressed: () {
+                Navigator.pop(context);
+              },
+              child: Text("Cancel"),
+            ),
+          ],
+        );
+      },
+    );
+  }
+
+  //pay button pressed
+  void payButtonPressed(BuildContext context) {
+    //show a dialog box to ask the user if they want to pay
+    showDialog(
+      context: context,
+      builder: (context) {
+        return const AlertDialog(
+          content: Text("Backend payment here"),
+        );
+      },
+    );
   }
 
   @override
@@ -52,26 +67,37 @@ class CartPage extends StatelessWidget {
         children: [
           //cart list
           Expanded(
-            child: ListView.builder(
-                itemCount: cart.length,
-                itemBuilder: (context, index) {
-                  //get each product
-                  final product = cart[index];
+            child: cart.isEmpty
+                ? const Center(child: Text("Cart is empty..."))
+                : ListView.builder(
+                    itemCount: cart.length,
+                    itemBuilder: (context, index) {
+                      //get each product
+                      final product = cart[index];
 
-                  //return a ProductTile
-                  return ListTile(
-                    title: Text(product.name),
-                    subtitle: Text(product.price.toStringAsFixed(2)),
-                    trailing: IconButton(
-                      icon: const Icon(Icons.remove),
-                      onPressed: () => removeFromCart(context, product),
-                    ),
-                  );
-                }),
+                      //return a ProductTile
+                      return ListTile(
+                        title: Text(product.name),
+                        subtitle: Text(product.price.toStringAsFixed(2)),
+                        trailing: IconButton(
+                          icon: const Icon(Icons.remove),
+                          onPressed: () => removeFromCart(context, product),
+                        ),
+                      );
+                    }),
           ),
           //total price
 
           //pay button
+          cart.isEmpty
+              ? const SizedBox()
+              : Padding(
+                  padding: const EdgeInsets.all(50.0),
+                  child: MyButton(
+                    onTap: () => payButtonPressed(context),
+                    child: Text("Pay now!"),
+                  ),
+                ),
         ],
       ),
     );
