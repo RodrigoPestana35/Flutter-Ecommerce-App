4158ffdf9342d15247229fa5944a39c7a8237612
RodrigoPestana35 <rodrigopestanaabreu@gmail.com>
2024-10-02 12:47:15 +0100

    Refactor MyDrawer component to include additional tiles for Shop, Cart, and Exit

--- lib/components/my_drawer.dart
+++ lib/components/my_drawer.dart
@@ -11,37 +11,60 @@ class MyDrawer extends StatelessWidget {
     return Drawer(
       backgroundColor: Theme.of(context).colorScheme.surface,
       child: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceBetween,
         children: [
-          //drawer header
-          DrawerHeader(
-            child: Center(
-              child: Icon(
-                Icons.shopping_bag_rounded,
-                size: 75,
-                color: Theme.of(context).colorScheme.inversePrimary,
+          Column(
+            children: [
+              //drawer header
+              DrawerHeader(
+                child: Center(
+                  child: Icon(
+                    Icons.shopping_bag_rounded,
+                    size: 75,
+                    color: Theme.of(context).colorScheme.inversePrimary,
+                  ),
+                ),
               ),
-            ),
-          ),
 
-          //shop tile
-          MyListTile(
-            text: "Shop",
-            icon: Icons.home,
-            onTap: () {},
-          ),
+              //shop tile
+              MyListTile(
+                text: "Shop",
+                icon: Icons.home,
+                onTap: () {
+                  //close drawer
+                  Navigator.pop(context);
+                },
+              ),
 
-          //cart tile
-          MyListTile(
-            text: "Cart",
-            icon: Icons.shopping_cart,
-            onTap: () {},
+              //cart tile
+              MyListTile(
+                text: "Cart",
+                icon: Icons.shopping_cart,
+                onTap: () {
+                  //close drawer
+                  Navigator.pop(context);
+
+                  //navigate to cart page
+                  Navigator.pushNamed(context, "/cart_page");
+                },
+              ),
+            ],
           ),
 
           //exit tile
-          MyListTile(
-            text: "Exit",
-            icon: Icons.exit_to_app,
-            onTap: () {},
+          Padding(
+            padding: const EdgeInsets.only(bottom: 25),
+            child: MyListTile(
+              text: "Exit",
+              icon: Icons.exit_to_app,
+              onTap: () {
+                //close drawer
+                Navigator.pop(context);
+
+                //exit app
+                Navigator.pop(context);
+              },
+            ),
           ),
         ],
       ),
